ef6dcc0cb71a543f7731fc4e4faa841f9c9a5499
Visharad Sandal <76243397+Visharad26@users.noreply.github.com>
2021-04-04 12:08:16 +0530

    Update 029_StudentArrStruct.c

--- 029_StudentArrStruct.c
+++ 029_StudentArrStruct.c
@@ -1,4 +1,4 @@
-//A C Program to store Array of structures for student marks for each student and passing it to function for various operations.          © Ishav Verma 20/March/2021
+//A C Program to store Array of structures for student marks for each student and passing it to function for various operations.         
 #include <stdio.h>
 #include <string.h>
  

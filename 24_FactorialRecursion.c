348be72482a3fb9cfaff1b8f6b408f1b95b59d44
Visharad Sandal <76243397+Visharad26@users.noreply.github.com>
2021-04-06 21:10:55 +0530

    Update 24_FactorialRecursion.c

--- 24_FactorialRecursion.c
+++ 24_FactorialRecursion.c
@@ -1,4 +1,4 @@
-//A C Program to print factorial using recursion.	 			© Ishav Verma 20/March/2021
+//A C Program to print factorial using recursion.	 			
 #include<stdio.h>
 	long int multiplyNumbers(int n);
 	int main() {

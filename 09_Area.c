09781f7d4fd8002a4c598633fd2f54f1053a8da9
Visharad Sandal <76243397+Visharad26@users.noreply.github.com>
2021-04-06 21:00:10 +0530

    Update 09_Area.c

--- 09_Area.c
+++ 09_Area.c
@@ -1,4 +1,4 @@
-//A C Program to calculate Area/Perimeter of different shapes.               © Ishav Verma 18/March/2021
+//A C Program to calculate Area/Perimeter of different shapes.               
 //Area of Square: side*side: side
 //Area of Rectangle: Length * Breadth: Length, Breadth
 //Area of Circle: (22/7)*r*r: radius 

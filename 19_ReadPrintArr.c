0811b5b9751dc919e0013b4d480e2b91da589631
Visharad Sandal <76243397+Visharad26@users.noreply.github.com>
2021-04-06 21:08:30 +0530

    Update 19_ReadPrintArr.c

--- 19_ReadPrintArr.c
+++ 19_ReadPrintArr.c
@@ -1,4 +1,4 @@
-// A C Program to read and print elements in two dimensional array. © Ishav Verma 19/March/2021
+// A C Program to read and print elements in two dimensional array. 
 #include<stdio.h>
 int main(){
    /* 2D array declaration*/
